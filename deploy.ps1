ba07ad3de4abd3af3aa0bbb5a209d0ec1f044a8c
guoluwei <guoluwei>
2018-12-20 19:25:46 +0800

    init

---
+++ deploy.ps1
@@ -0,0 +1,14 @@
+
+
+npm run docs:build
+
+Set-Location docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:glwlg/glwlg.github.io.git master
+
+Set-Location ..
+Remove-Item dist -Force  -recurse
